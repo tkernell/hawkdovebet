2647fe4e2c45a58d7da1dceb17f4ad6e68d732e0
tkernell <tpkernell@gmail.com>
2018-10-25 23:54:00 -0500

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -1,40 +1,42 @@
 pragma solidity ^0.4.25;
 
-// This is incomplete and probably buggy
-
 contract HawkDoveBet {
     
     address public person1;
     address public person2;
     address public owner;
-    uint256 betAmount;
-    uint256 stakeAmount;
-    uint256 deadline;
-    bool inGame;
+    uint256 public betAmount;
+    uint256 public stakeAmount;
+    uint256 private totalAmount;
+    uint256 public deadline;
+    bool public inGame;
+    address private myAddress = this;
+    address private burnAddress = 0x0000000000000000000000000000000000000000;
     
+    // Mapping to keep track of players' respective balances
     mapping(address => uint256) public balanceOf;
-    mapping(address => bool) public didWin;
+    mapping(address => bool) public didWin; 
     
     modifier onlyOwner {
         require(msg.sender == owner);
         _;
     }
-    
     modifier onlyPlayer {
         require(msg.sender == person1 || msg.sender == person2);
         _;
     }
-    
     modifier onlyInGame {
-        require(inGame == true);
+        require(inGame);
         _;
     }
-    
+    modifier onlyNotInGame {
+        require(!inGame);
+        _;
+    }
     modifier onlyBeforeDeadline {
         require(now < deadline);
         _;
     }
-    
     modifier onlyAfterDeadline {
         require(now >= deadline);
         _;
@@ -52,6 +54,7 @@ contract HawkDoveBet {
             person2 = _person2;
             betAmount = _betAmount * 1 finney;
             stakeAmount = _stakeAmount * 1 finney;
+            totalAmount = betAmount + stakeAmount;
             deadline = now + _deadlineHours * 1 hours;
         }
     
@@ -62,20 +65,48 @@ contract HawkDoveBet {
     }
     
     function beginGame() onlyPlayer public {
-        uint totalAmount = betAmount + stakeAmount;
+        //uint totalAmount = betAmount + stakeAmount;
         require(balanceOf[person1] >= totalAmount && balanceOf[person2] >= totalAmount);
         inGame = true;
     }
     
-    function iWin() onlyPlayer onlyInGame public {
+    // A player declares victory. Only allowed during game and before deadline
+    function iWin() onlyPlayer onlyInGame onlyBeforeDeadline public {
         didWin[msg.sender] = true;
     }
     
-    function iLose() onlyPlayer onlyInGame public {
+    // A player declares a loss. Only allowed during game and before deadline
+    function iLose() onlyPlayer onlyInGame onlyBeforeDeadline public {
         didWin[msg.sender] = false;
     }
     
-    function settleGame() 
+    function settleGame() onlyAfterDeadline onlyInGame public {
+        if (didWin[person1] != didWin[person2]) {
+            if (didWin[person1]) {      //If p1 won, update bal
+                balanceOf[person1] += betAmount;
+                balanceOf[person2] -= betAmount;
+                inGame = false;
+            } else {                    //If p2 won, update bal
+                balanceOf[person2] += betAmount;
+                balanceOf[person1] -= betAmount;
+                inGame = false;
+            }
+        } else if (!didWin[person1]) {  //If no winner, end game
+            inGame = false;
+        } else {                        //If both claim win, burn
+            uint burnAmount = totalAmount * 2;
+            burnAddress.transfer(burnAmount);
+            balanceOf[person1] -= totalAmount;
+            balanceOf[person2] -= totalAmount;
+            inGame = false;
+        }
+    }
     
+    function withdrawBalance() onlyPlayer onlyNotInGame public {
+        require(balanceOf[msg.sender] > 0);
+        uint tempBal = balanceOf[msg.sender];
+        balanceOf[msg.sender] = 0;
+        msg.sender.transfer(tempBal);
+    }
     
 }
